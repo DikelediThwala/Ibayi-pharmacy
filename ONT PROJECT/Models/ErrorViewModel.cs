dbda5a1647e32469a632b627ac62ce774c2579b0
Dikeledi <thwaladikeledi15@gmail.com>
2025-04-29 10:50:32 +0200

    Add project files.

---
+++ ONT PROJECT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ONT_PROJECT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
