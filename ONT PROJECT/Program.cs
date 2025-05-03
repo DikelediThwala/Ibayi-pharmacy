9ad2d5c487bd71a55f2921c43f30b078f974075f
Dikeledi <thwaladikeledi15@gmail.com>
2025-05-03 21:16:17 +0200

    FAQ

--- ONT PROJECT/Program.cs
+++ ONT PROJECT/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ONT_PROJECT.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
