da7a0caa5f1d67eeb954e0bd4e0589746050ec9d
Dikeledi <thwaladikeledi15@gmail.com>
2025-05-03 21:13:57 +0200

    Supplier

---
+++ ONT PROJECT/Controllers/SupplierController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ONT_PROJECT.Models;
+using Microsoft.EntityFrameworkCore; // Add this
+
+
+namespace ONT_PROJECT.Controllers
+{
+    public class SupplierController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SupplierController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+    }
+
+}
---
+++ ONT PROJECT/Models/Supplier.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ONT_PROJECT.Models
+{
+    public class Supplier
+    {
+        public int SupplierID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(11)]
+        public string ContactNo { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(50)]
+        public string Email { get; set; }
+    }
+}
+
