034f0f8b038b814b9cbdcf4fc0c9af5d6ce04f1b
Dikeledi <thwaladikeledi15@gmail.com>
2025-05-03 21:13:38 +0200

    Pharmacy

---
+++ ONT PROJECT/Controllers/PharmacyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ONT_PROJECT.Controllers
+{
+    public class PharmacyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ ONT PROJECT/Models/Pharmacy.cs
@@ -0,0 +1,51 @@
+﻿using ONT_PROJECT.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ONT_PROJECT.Models
+{
+    public class Pharmacy
+    {
+        [Key]
+        public int PharmacyID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Health Council Registration No")]
+        public string HealthCounsilRegistrationNo { get; set; }
+
+        [Required]
+        [StringLength(11)]
+        [Display(Name = "Contact Number")]
+        public string ContactNo { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        [Url]
+        [Display(Name = "Website URL")]
+        public string WebsiteUrl { get; set; }
+
+        [Required]
+        [ForeignKey("Pharmacist")]
+        public int PharmacistID { get; set; }
+
+        public byte[] Logo { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "VAT Rate")]
+        public float VATRate { get; set; }
+
+        // Navigation property
+        //public Pharmacist Pharmacist { get; set; }
+    }
+}
