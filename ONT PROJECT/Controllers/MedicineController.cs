f42fc145c081aa1f979bc8e93e5925d38b70be49
Dikeledi <thwaladikeledi15@gmail.com>
2025-05-03 21:13:16 +0200

    Medicine

---
+++ ONT PROJECT/Controllers/MedicineController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ONT_PROJECT.Controllers
+{
+    public class MedicineController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ ONT PROJECT/Models/Medicine.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ONT_PROJECT.Models
+{
+    public class Medicine
+    {
+        [Key]
+        public int MedicineID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string MedicineName { get; set; }
+
+        [Required]
+        public int Schedule { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Ingredients { get; set; }
+
+        [Required]
+        public float SalesPrice { get; set; }
+
+        [Required]
+        public int SupplierID { get; set; }
+
+        [Required]
+        public int ReorderLevel { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public int FormID { get; set; }
+
+        // Navigation property to the Supplier model
+        [ForeignKey("SupplierID")]
+        public Supplier Supplier { get; set; }
+    }
+
+}
