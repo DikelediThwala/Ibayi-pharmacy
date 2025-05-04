4c01bb419d08193b71f3a022fd7b9e1bc92fd6e5
Dikeledi <thwaladikeledi15@gmail.com>
2025-05-04 19:34:06 +0200

    Doctor

---
+++ ONT PROJECT/Models/Doctor.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ONT_PROJECT.Models
+{
+    public class Doctor
+    {
+        public int DoctorID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public string PracticeNo { get; set; }
+
+        [Required]
+        [Phone]
+        public string ContactNo { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+     
+    }
+}
