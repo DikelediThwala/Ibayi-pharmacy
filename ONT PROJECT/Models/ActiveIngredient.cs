2323bcf456a967b560d1a2a046ed205dcbcff2b9
Dikeledi <thwaladikeledi15@gmail.com>
2025-05-04 19:23:59 +0200

    Active Ingredents

---
+++ ONT PROJECT/Models/ActiveIngredient.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ONT_PROJECT.Models
+{
+    public class ActiveIngredient
+    {
+        public int ActiveIngredientsID { get; set; }
+
+        [Required]
+        public string Ingredients { get; set; }
+    }
+}
