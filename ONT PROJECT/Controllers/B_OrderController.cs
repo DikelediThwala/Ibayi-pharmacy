7fe15f72ddcdfbbc8d6f8dcf5a9eaa5c6c722955
Dikeledi <thwaladikeledi15@gmail.com>
2025-05-04 19:26:58 +0200

    B_Order

---
+++ ONT PROJECT/Controllers/B_OrderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ONT_PROJECT.Controllers
+{
+    public class B_OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ ONT PROJECT/Models/B_Order.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ONT_PROJECT.Models
+{
+    public class B_Order
+    {
+        [Key]
+        public int B_OrderID { get; set; }
+
+        [Required]
+        public DateOnly DatePlaced { get; set; }
+
+        [Required]
+        public DateOnly DateRecieved { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Status { get; set; }
+
+        [ForeignKey("PharmacyManagerID")]
+        public Supplier PharmacyManagerID { get; set; }
+
+        [ForeignKey("SupplierID")]
+        public Supplier SupplierID { get; set; }
+    }
+}
