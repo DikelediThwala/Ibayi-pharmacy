32009e779e65865a4dc463b2f8854fe5f6348819
s224113828 <s224113828@R128-13>
2025-05-21 12:41:40 +0200

    Pushh

---
+++ ONT PROJECT/Controllers/CustomerReport.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ONT_PROJECT.Controllers
+{
+    public class CustomerReport : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
