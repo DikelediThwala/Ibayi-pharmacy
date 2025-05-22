b8b5e92cd8b35e2f306db25470950ef9a8c2366a
Dikeledi <thwaladikeledi15@gmail.com>
2025-05-22 17:36:23 +0200

    Manager

--- ONT PROJECT/Controllers/B_OrderController.cs
+++ ONT PROJECT/Controllers/B_OrderController.cs
@@ -1,16 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ONT_PROJECT.Models;
 
 namespace ONT_PROJECT.Controllers
 {
     public class B_OrderController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public B_OrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //public async Task<IActionResult> Index()
+        //{
+        //    var orders = await _context.B_Orders
+        //        .Include(o => o.Supplier)
+        //        .Include(o => o.OrderLines)
+        //            .ThenInclude(ol => ol.Medication)
+        //        .ToListAsync();
+
+        //    return View(orders);
+        //}
+
+
         public IActionResult Index()
         {
             return View();
         }
+
         public IActionResult Create()
+        {
+            
+            return View();
+        }
+        public IActionResult OutOfStock()
         {
             return View();
         }
+        public IActionResult Selected()
+        {
+            return View();
+        }
+
     }
 }
--- ONT PROJECT/Models/ApplicationDbContext.cs
+++ ONT PROJECT/Models/ApplicationDbContext.cs
@@ -11,5 +11,6 @@ namespace ONT_PROJECT.Models
         }
 
         public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<B_Order>B_Orders { get; set; }
     }
 }
