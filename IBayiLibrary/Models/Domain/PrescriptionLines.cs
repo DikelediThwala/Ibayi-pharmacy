2322f439e00dca18e9d9a044d67695b7ff12c7ec
s225086883 <s225086883@M119-30>
2025-08-06 14:34:05 +0200

    Prescription Line

---
+++ IBayiLibrary/Models/Domain/PrescriptionLines.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBayiLibrary.Models.Domain
+{
+    public class PrescriptionLines
+    {
+        public int PrescrptionLineID {  get; set; }
+        public int PrescriptionID { get; set; }
+        [ForeignKey("MedicineID")]
+        public int MedicineID { get;set; }
+        public string Instructions { get; set; }
+        public int Quantity { get;set; }
+        public int Repeats { get; set; }
+        public int RepeatsLeft { get; set; }
+
+    }
+}
---
+++ IBayiLibrary/Repository/PrescriptionLineRepository.cs
@@ -0,0 +1,44 @@
+﻿using IBayiLibrary.DataAccess;
+using IBayiLibrary.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBayiLibrary.Repository
+{
+    public class PrescriptionLineRepository:IPrescriptionLineRepository
+    {
+        private readonly ISqlDataAccess _db;
+
+        public PrescriptionLineRepository(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> AddAsync(PrescriptionLines prescriptionLine)
+        {
+            try
+            {
+                await _db.SaveData("spInsertPrescriptionLine", new { prescriptionLine.PrescriptionID, prescriptionLine.MedicineID, prescriptionLine.Quantity, prescriptionLine.Instructions, prescriptionLine.Repeats,prescriptionLine.RepeatsLeft });
+                return true;
+            }
+
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public async Task<IEnumerable<Medicine>> GetMedicineName()
+        {
+            string query = "spGetMedicineName";
+            return await _db.GetData<Medicine, dynamic>(query, new { });
+        }
+        public async Task<IEnumerable<PrescriptionLines>> GetLastPrescriptioRow()
+        {
+            string query = "spGetLastPrescriptioRow";
+            return await _db.GetData<PrescriptionLines, dynamic>(query, new { });
+        }
+    }
+}
