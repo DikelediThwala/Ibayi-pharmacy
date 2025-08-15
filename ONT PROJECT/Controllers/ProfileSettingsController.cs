8a99ebe4a7cc87f08b7d1465c9caed5dc879af85
s224113828 <s224113828@R226-17>
2025-08-15 12:42:33 +0200

    profile settings

--- ONT PROJECT/Controllers/ProfileSettingsController.cs
+++ ONT PROJECT/Controllers/ProfileSettingsController.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using ONT_PROJECT.Models;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ONT_PROJECT.Models;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,19 +21,8 @@ namespace ONT_PROJECT.Controllers
         }
 
         // Show list of all customers (report)
-        public IActionResult Index()
-        {
-            var email = HttpContext.Session.GetString("UserEmail");
-            if (email == null) return RedirectToAction("Login", "CustomerRegister");
-
-            var user = _context.TblUsers.FirstOrDefault(u => u.Email == email);
-            if (user == null) return NotFound();
-
-            return View(user);
-        }
-
         [HttpGet]
-        public IActionResult Edit()
+        public IActionResult Index(bool? edit)
         {
             var email = HttpContext.Session.GetString("UserEmail");
             if (email == null) return RedirectToAction("Login", "CustomerRegister");
@@ -40,11 +30,38 @@ namespace ONT_PROJECT.Controllers
             var user = _context.TblUsers.FirstOrDefault(u => u.Email == email);
             if (user == null) return NotFound();
 
+            var customer = _context.Customers.FirstOrDefault(c => c.CustomerNavigation.UserId == user.UserId);
+            var selectedAllergies = new List<int>();
+            if (customer != null)
+            {
+                selectedAllergies = _context.CustomerAllergies
+                    .Where(ca => ca.CustomerId == customer.CustomerId)
+                    .Select(ca => ca.ActiveIngredientId)
+                    .ToList();
+            }
+
+            var allAllergies = _context.ActiveIngredient
+                .Select(ai => new SelectListItem
+                {
+                    Value = ai.ActiveIngredientId.ToString(),
+                    Text = ai.Ingredients
+                })
+                .OrderBy(a => a.Text)
+                .ToList();
+
+            ViewBag.SelectedAllergies = selectedAllergies;
+            ViewBag.ActiveIngredients = allAllergies;
+
+            // Toggle edit mode
+            ViewBag.EditMode = edit ?? false;
+
             return View(user);
         }
+
 
         [HttpPost]
-        public IActionResult Edit(TblUser model)
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(TblUser model, List<int> SelectedAllergyIds)
         {
             var email = HttpContext.Session.GetString("UserEmail");
             if (email == null) return RedirectToAction("Login", "CustomerRegister");
@@ -52,18 +69,77 @@ namespace ONT_PROJECT.Controllers
             var user = _context.TblUsers.FirstOrDefault(u => u.Email == email);
             if (user == null) return NotFound();
 
+            // Update user fields
             user.FirstName = model.FirstName;
             user.LastName = model.LastName;
             user.Idnumber = model.Idnumber;
             user.PhoneNumber = model.PhoneNumber;
-            user.Allergies = model.Allergies;
+
+            // Update allergies
+            var customer = _context.Customers.FirstOrDefault(c => c.CustomerNavigation.UserId == user.UserId);
+            if (customer != null)
+            {
+                // Remove all existing allergies
+                var existingAllergies = _context.CustomerAllergies
+                    .Where(ca => ca.CustomerId == customer.CustomerId)
+                    .ToList();
+                _context.CustomerAllergies.RemoveRange(existingAllergies);
+
+                // Add selected allergies
+                if (SelectedAllergyIds != null)
+                {
+                    foreach (var allergyId in SelectedAllergyIds)
+                    {
+                        _context.CustomerAllergies.Add(new CustomerAllergy
+                        {
+                            CustomerId = customer.CustomerId,    // important!
+                            ActiveIngredientId = allergyId
+                        });
+                    }
+                }
+            }
 
             _context.SaveChanges();
 
-            // ✅ Save success message in TempData
             TempData["SuccessMessage"] = "Your profile was updated successfully.";
 
             return RedirectToAction("Index");
         }
+
+
+        //[HttpGet]
+        //public IActionResult Edit()
+        //{
+        //    var email = HttpContext.Session.GetString("UserEmail");
+        //    if (email == null) return RedirectToAction("Login", "CustomerRegister");
+
+        //    var user = _context.TblUsers.FirstOrDefault(u => u.Email == email);
+        //    if (user == null) return NotFound();
+
+        //    return View(user);
+        //}
+
+        //[HttpPost]
+        //public IActionResult Edit(TblUser model)
+        //{
+        //    var email = HttpContext.Session.GetString("UserEmail");
+        //    if (email == null) return RedirectToAction("Login", "CustomerRegister");
+
+        //    var user = _context.TblUsers.FirstOrDefault(u => u.Email == email);
+        //    if (user == null) return NotFound();
+
+        //    user.FirstName = model.FirstName;
+        //    user.LastName = model.LastName;
+        //    user.Idnumber = model.Idnumber;
+        //    user.PhoneNumber = model.PhoneNumber;
+        //    user.Allergies = model.Allergies;
+
+        //    _context.SaveChanges();
+
+        //    // ✅ Save success message in TempData
+        //    TempData["SuccessMessage"] = "Your profile was updated successfully.";
+
+        //    return RedirectToAction("Index");
+        //}
     }
 }
