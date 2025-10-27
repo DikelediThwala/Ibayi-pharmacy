c4ae5b5ef9fe95a4411ddd3dbc5a1ce865268217
thwaladikeledi15@gmail.com <thwaladikeledi15@gmail.com>
2025-10-27 13:57:27 +0200

    .

--- ONT PROJECT/Models/TblUser.cs
+++ ONT PROJECT/Models/TblUser.cs
@@ -53,7 +53,7 @@ public partial class TblUser
     public List<int> SelectedAllergyIds { get; set; } = new();
     public string? ResetToken { get; set; }
     public DateTime? TokenExpiry { get; set; }
-    [NotMapped] // EF will ignore this
+    [NotMapped] 
     public string HealthCounsilRegNo { get; set; }
     public byte[]? ProfilePicture { get; set; }
 
