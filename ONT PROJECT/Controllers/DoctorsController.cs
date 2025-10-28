2b34c540d1aaab46e209b25b1eb862b6511470f8
Asenathi03 <yawaasenathi2003@gmail.com>
2025-10-28 15:16:27 +0200

    djsjfdh

--- ONT PROJECT/Controllers/DoctorsController.cs
+++ ONT PROJECT/Controllers/DoctorsController.cs
@@ -50,9 +50,79 @@ namespace ONT_PROJECT.Controllers
             {
                 TempData["msg"] = " Something went wrong!!!";
             }
-            return RedirectToAction("LoadPrescription","Pharmacist");
+            return RedirectToAction("CreatePrescForWalkins", "UploadPrescription");
         }
-       
+        public IActionResult CreateDoctorForImmediateDispense()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateDoctorForImmediateDispense(Doctor doctor)
+        {
+            try
+            {
+                bool emailExists = await _doctorRepository.CheckEmailExistsAsync(doctor.Email);
+                if (emailExists)
+                {
+                    ModelState.AddModelError("Email", "This email address is already registered.");
+                    TempData["msg"] = "Email already exists!";
+                    return View(doctor);
+                }
 
+
+                bool addPerson = await _doctorRepository.AddAsync(doctor);
+                if (addPerson)
+                {
+                    TempData["msg"] = "Sucessfully Added";
+                }
+                else
+                {
+                    TempData["msg"] = "Could not add";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = " Something went wrong!!!";
+            }
+            return RedirectToAction("CreatePrescForImmediateDispense", "UploadPrescription");
+        }
+        public IActionResult CreateDoctorForPresc()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateDoctorForPresc(Doctor doctor)
+        {
+            try
+            {
+                bool emailExists = await _doctorRepository.CheckEmailExistsAsync(doctor.Email);
+                if (emailExists)
+                {
+                    ModelState.AddModelError("Email", "This email address is already registered.");
+                    TempData["msg"] = "Email already exists!";
+                    return View(doctor);
+                }
+
+
+                bool addPerson = await _doctorRepository.AddAsync(doctor);
+                if (addPerson)
+                {
+                    TempData["msg"] = "Sucessfully Added";
+                }
+                else
+                {
+                    TempData["msg"] = "Could not add";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = " Something went wrong!!!";
+            }
+            return RedirectToAction("CreatePrescriptions", "UploadPrescription");
+        }
     }
 }
